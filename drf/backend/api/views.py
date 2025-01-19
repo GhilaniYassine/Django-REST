1f3eeb5db48ed89ea10e7c9f5bf525cc60a2b927
GhilaniYassine <ghilaniyassine11@gmail.com>
2025-01-19 04:26:49 -0800

    the first api app is done mmoving on to the next django model instance as an api response

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,7 +1,27 @@
+
+import json 
+
 from  django.http import JsonResponse
 
 
-def api_home(requests  , *args , **kwargs):
-    return JsonResponse({"message" : " HI THERE ITS yassine api's "})
+def api_home(request , *args , **kwargs): # this params request is a instance of Httprequests from django
 
+    body = request.body #byte string of json data 
+    data = {}
+    try :
+        data = json.loads(body) #takes a string of jason data and turns into a python dict 
+
+    except : ## the reason behinde the try is bloc is that it is possible that the body its not a contains a json  
+
+        pass
+
+    print(data.keys)
+    
+    #data['headers'] = request.headers # back then was request.META
+    data['headers'] = dict(request.headers  )
+    data['content_type']= request.content_type
+    data['params'] = dict(request.GET)
+    print(request.headers)
+    return JsonResponse(data)
+    
 
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -3,11 +3,12 @@ import requests
 endpoint= " http://127.0.0.1:8000/api/" # if you are not running  the server with the commande pyhton manage.py runserver you want get response from this particulare endpoint 
 
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint , params= {"abc" : 123} , json = {"query" : " hello world!"})
 
-print(get_response.text) #the raw text response the html
-print(get_response.json()['message']) # to get only the message as json similar to the python dict 
+#print(get_response.text) #the raw text response the html
+print(get_response.json() ) # to get only the message as json similar to the python dict 
 
-print(get_response.status_code)
+#print(get_response.status_code)
 
 
+ ## echo things form a given  somthing 
