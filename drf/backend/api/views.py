70be33d85ad92278f10396199a32fd24f05836f7
GhilaniYassine <ghilaniyassine11@gmail.com>
2025-01-20 01:02:38 -0800

    MOVING TO THE DJANGO MODEL INSTANCE TO DICT

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,27 +1,21 @@
 
-import json 
+import json  # to change the data from json to python dict 
+from product.models import Product
 
 from  django.http import JsonResponse
 
 
 def api_home(request , *args , **kwargs): # this params request is a instance of Httprequests from django
 
-    body = request.body #byte string of json data 
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try :
-        data = json.loads(body) #takes a string of jason data and turns into a python dict 
-
-    except : ## the reason behinde the try is bloc is that it is possible that the body its not a contains a json  
-
-        pass
-
-    print(data.keys)
-    
-    #data['headers'] = request.headers # back then was request.META
-    data['headers'] = dict(request.headers  )
-    data['content_type']= request.content_type
-    data['params'] = dict(request.GET)
-    print(request.headers)
+    if model_data : 
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content 
+        data['price'] = model_data.price
+        """we will do the serialization means
+        1/ model instance 
+         2/ trun it to py dict 
+          3/ return json to my client  """
     return JsonResponse(data)
-    
-
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -1,7 +1,7 @@
 import requests 
 
 endpoint= " http://127.0.0.1:8000/api/" # if you are not running  the server with the commande pyhton manage.py runserver you want get response from this particulare endpoint 
-
+#endpoint_google = "https://www.google.com/search?q=google+currency+converter&oq=google+curre&gs_lcrp=EgZjaHJvbWUqDAgAEAAYRhiCAhiABDIMCAAQABhGGIICGIAEMgYIARBFGDkyBwgCEAAYgAQyBwgDEAAYgAQyBwgEEAAYgAQyBwgFEAAYgAQyBwgGEAAYgAQyBwgHEAAYgAQyBwgIEAAYgAQyBwgJEAAYgATSAQgzNDk2ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8"
 
 get_response = requests.get(endpoint , params= {"abc" : 123} , json = {"query" : " hello world!"})
 
